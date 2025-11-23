acf9085da4afb722e745323d733bd02ebe879b39
Abhilash K K <abhilashkk1857@gmail.com>
2025-11-23 22:41:29 +0530

    fix(security): Handle invalid JWTs gracefully to prevent 500 errors

--- src/main/java/com/kk/blog_app/config/JwtAuthenticationFilter.java
+++ src/main/java/com/kk/blog_app/config/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -27,16 +29,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
-        String email;
-        String token;
+        String email = null;
+        String token = null;
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request,response);
             return;
         }
 
-        token = authHeader.substring(7);
-        email = jwtService.extractEmail(token);
+        try {
+            token = authHeader.substring(7);
+            email = jwtService.extractEmail(token);
+        }
+        catch (Exception exception) {
+            log.error("JWT Authentication failed: {}", exception.getMessage());
+        }
 
         // validation
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
